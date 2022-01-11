b64486e19ecdd64b913ff13d7a129fd583bcced9
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2022-01-11 15:27:56 +0100

    added stringtie

--- StringTie/flake.nix
+++ StringTie/flake.nix
@@ -1,12 +1,12 @@
 {
   description = "StringTie RNAseq assembler";
 
-  inputs.nixpkgs.url = "nixpkgs/nixos-21.05"; # doesn't matter much
+  inputs.nixpkgs.url = "nixpkgs/nixos-21.11"; # we need htslib > 1.11
 
   outputs = { self, nixpkgs }:
     let
 
-      # Generate a user-friendly version number.
+      # Generate aa user-friendly version number.
       #version = builtins.substring 0 8 self.lastModifiedDate;
 
       supportedSystems = [
@@ -17,28 +17,49 @@
       ]; # it's java afterall
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+      version_hashes = {
+        "2.0.3" = "sha256-FSyZ4qxczIPP8+pVo8hjcVFZarQhWhehG7Nq1HW9S0I=";
+        "2.0.6" = "sha256-Mi1cv2r4HjBjJmaIBk0rlipLsUrbWCPvVQN22R1v+R0=";
+        "2.2.0" = "sha256-fUQ1FDrfH+n2Bs5jU0ORRvWIIo9ozHOZSm4mT6COgyM=";
+      };
 
-    in {
+    in rec {
 
       # package.
-      defaultPackage = forAllSystems (system:
+      stringtie = forAllSystems (system: version:
         let pkgs = nixpkgsFor.${system};
         in pkgs.stdenv.mkDerivation rec {
           pname = "StringTie";
-          version = "2.0.3";
+          inherit version;
           src = pkgs.fetchurl {
             url =
-              "http://ccb.jhu.edu/software/stringtie/dl/stringtie-2.2.0.tar.gz";
-            sha256 = pkgs.lib.fakeSha256;
+              "http://ccb.jhu.edu/software/stringtie/dl/stringtie-${version}.tar.gz";
+            sha256 = version_hashes.${version} or pkgs.lib.fakeSha256;
           };
-          nativeBuildInputs = [];
+          nativeBuildInputs = with pkgs; [
+            zlib
+            htslib
+            bzip2
+            libdeflate
+            xz
+            curl.dev
+            openssl.dev
+          ];
+          patches = [ ./patch-2.2.0.patch ];
           buildPhase = ''
-            make release
+            set -euo pipefail
+              export HTSLIB=${pkgs.htslib}/lib;
+              export LIBDEFLATE=${pkgs.libdeflate}/lib/libdeflate.a;
+              export LIBBZ2=-lbz2;
+              export LIBLZMA=-llzma;
+              make release
           '';
           installPhase = ''
-          mkdir $out/bin -p
-          cp ../bin/* $out/bin/
+            mkdir $out/bin -p
+            cp stringtie $out/bin/
           '';
         });
+      defaultPackage = forAllSystems (system: (stringtie.${system} "2.2.0"));
+
     };
 }
