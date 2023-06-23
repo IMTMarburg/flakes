7273d79c07efa83395f8793fd62c1a3a4c8d99b2
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2023-06-23 08:20:31 +0200

    SCSA meta data

--- SCSA/flake.nix
+++ SCSA/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Velvet assembler";
+  description = "SCSA single cell annotation";
 
   inputs.nixpkgs.url = "nixpkgs/nixos-23.05"; # I'm pretty sure it will start failing once pands is newer than 1.5.x
 
@@ -20,7 +20,7 @@
     defaultPackage = forAllSystems (
       system: let
         pkgs = nixpkgsFor.${system};
-        mypython = (pkgs.python3.withPackages (ps: with ps; [numpy scipy openpyxl pandas]));
+        mypython = pkgs.python3.withPackages (ps: with ps; [numpy scipy openpyxl pandas]);
       in
         pkgs.stdenv.mkDerivation rec {
           pname = "SCSA";
@@ -43,6 +43,16 @@
             mkdir $out/share/ -p
             cp *.db $out/share/
           '';
+          meta = with lib; {
+            homepage = "https://github.com/bioinfo-ibms-pumc/SCSA";
+            description = "SCSA: cell type annotation for single-cell RNA-seq data";
+            longDescription = ''
+              Currently most methods take manual strategies to annotate cell types after clustering the single-cell RNA-seq data. Such methods are labor-intensive and heavily rely on user expertise, which may lead to inconsistent results. We present SCSA, an automatic tool to annotate cell types from single-cell RNA-seq data, based on a score annotation model combining differentially expressed genes and confidence levels of cell markers in databases. Evaluation on real scRNA-seq datasets that SCSA is able to assign the cells into the correct types at a fully automated mode with a desirable precision.
+            '';
+            license = licenses.gpl3;
+            #maintainers = with maintainers; [ AndersonTorres ];
+            platforms = platforms.unix;
+          };
         }
     );
   };
