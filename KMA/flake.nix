85522bdbd61c519cffc30256b362ca2d85466510
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2023-10-19 10:40:59 +0200

    set kma version to what it reports

--- KMA/flake.nix
+++ KMA/flake.nix
@@ -18,7 +18,7 @@
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
       version_hashes = {
-        "83d8899c089d1d4b81d555b949086ad771b5654a" = "sha256-OMuPSEg/7hQEp2zqOB1rm7cCaNv6VgMSnXQHNppdV3U=";
+        "1.4.14" = "sha256-OMuPSEg/7hQEp2zqOB1rm7cCaNv6VgMSnXQHNppdV3U="; # kma -v reported version...
         #"2.0.3" = "sha256-Vs7zovkU1DJxMGnVwoL0iDHDoezIlDKtVYDKoyKl9Ws=";
       };
 
@@ -42,6 +42,6 @@
             cp kma $out/bin/ || true
           '';
         });
-      defaultPackage = forAllSystems (system: (subread.${system} "83d8899c089d1d4b81d555b949086ad771b5654a"));
+      defaultPackage = forAllSystems (system: (subread.${system} "1.4.14"));
     };
 }
