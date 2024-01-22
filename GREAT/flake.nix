f9901dfba72ec4cc080f33a2f50b502abaf4901b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-01-22 11:25:22 +0100

    GREAT now imports curated regulatory control regions

--- GREAT/flake.nix
+++ GREAT/flake.nix
@@ -23,7 +23,20 @@
   in rec {
     salmon = forAllSystems (
       system: version: let
+        lib = pkgs.lib;
         pkgs = nixpkgsFor.${system};
+      in let
+        curated_regulatory_domains = {
+          "hg38" = pkgs.fetchurl {
+            url = "https://great-help.atlassian.net/wiki/download/attachments/655443/GREATv4.curatedRegDoms.hg38.txt?version=1&modificationDate=1627413265059&cacheVersion=1&api=v2&download=true";
+            sha256 = "sha256-9jzJhv0z4Gy+8MrYxNmYt2v1E7hn943LOWF9naA19CY=";
+          };
+
+          "mm10" = pkgs.fetchurl {
+            url = "https://great-help.atlassian.net/wiki/download/attachments/655443/GREATv4.curatedRegDoms.mm10.txt?version=1&modificationDate=1627413266154&cacheVersion=1&api=v2&download=true";
+            sha256 = "sha256-er10QsHhJwUpyKC1utFULjN5Cli4fT27pzRUTt/UcSs=";
+          };
+        };
       in
         pkgs.stdenv.mkDerivation {
           pname = "GREAT";
@@ -32,19 +45,28 @@
             url = "http://bejerano.stanford.edu/resources/greatTools.tar.gz";
             sha256 = version_hashes.${version} or pkgs.lib.fakeSha256;
           };
-          postPatch= ''
-          substituteInPlace makefile --replace "path/to/your/kent/src" "${pkgs.kent}" \
-            --replace "\$(KENT_DIR)/lib/\$(MACHTYPE)/jkweb.a" "${pkgs.kent}/lib/jkweb.a" \
-            --replace "LDFLAGS=" "LDFLAGS=-L${pkgs.openssl.out}/lib" \
-            --replace "-lm" "-lm -lssl -lcrypto -lhts"
-            cat makefile
+          postPatch = ''
+            substituteInPlace makefile --replace "path/to/your/kent/src" "${pkgs.kent}" \
+              --replace "\$(KENT_DIR)/lib/\$(MACHTYPE)/jkweb.a" "${pkgs.kent}/lib/jkweb.a" \
+              --replace "LDFLAGS=" "LDFLAGS=-L${pkgs.openssl.out}/lib" \
+              --replace "-lm" "-lm -lssl -lcrypto -lhts"
+              cat makefile
           '';
           nativeBuildInputs = [pkgs.kent pkgs.htslib pkgs.openssl.dev];
-          installPhase = ''
-            mkdir $out/bin -p
-            cp calculateBinomialP $out/bin
-            cp createRegulatoryDomains $out/bin
-          '';
+          installPhase =
+            ''
+              mkdir $out/bin -p
+              cp calculateBinomialP $out/bin
+              cp createRegulatoryDomains $out/bin
+
+              mkdir $out/curated_regulatory_regions
+            ''
+            + (builtins.concatStringsSep "\n" (
+              lib.mapAttrsToList (name: url_pkg: ''
+                cp ${url_pkg} $out/curated_regulatory_regions/${name}.tsv
+              '')
+              curated_regulatory_domains
+            ));
           meta = {
             description = "GREAT ChipSeq overlap analysis";
             homepage = "https://great-help.atlassian.net/wiki/spaces/GREAT/pages/655416/Download";
