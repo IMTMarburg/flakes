97a097de4f1f2f2395525e2a706688353de32ed1
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-04-26 15:05:02 +0200

    meme: perl dependncies for subprograms

--- meme-suite/default.nix
+++ meme-suite/default.nix
@@ -102,6 +102,22 @@
       license = with lib.licenses; [artistic1 gpl1Plus];
     };
   };
+  myperl = with perlPackages;
+    makePerlPath [
+      XMLSimple
+      XMLParser
+      SysInfo
+      ScalarListUtils
+      ListAllUtils
+      PodUsage
+      JSON
+      HTMLTree
+      HTMLTemplate
+      GetoptLong
+      FileTemp
+      PathTools
+      DataDumper
+    ];
 in
   stdenv.mkDerivation rec {
     pname = "meme-suite";
@@ -115,23 +131,49 @@ in
     buildInputs = [zlib];
     nativeBuildInputs = [perl python3 makeWrapper];
     postFixup = ''
-      wrapProgram $out/bin/meme-chip \
-      --prefix PERL5LIB : "${with perlPackages;
-        makePerlPath [
-          XMLSimple
-          XMLParser
-          SysInfo
-          ScalarListUtils
-          ListAllUtils
-          PodUsage
-          JSON
-          HTMLTree
-          HTMLTemplate
-          GetoptLong
-          FileTemp
-          PathTools
-          DataDumper
-        ]}"
+      wrapProgram $out/bin/meme-chip --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/ama-qvalues --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/beeml2meme --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/centrimo-plots --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/chen2meme --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/dreme_xml_to_html --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/dreme_xml_to_txt --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/elm2meme --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/fasta-center --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/fasta-fetch --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/fasta-grep --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/fasta-make-index --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/fasta-most --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/fasta-re-match --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/fasta-subsample --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/fasta-unique-names --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/hart2meme-bkg --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/hartemink2psp --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/iupac2meme --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/jaspar2meme --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/mast_xml_to_html --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/mast_xml_to_txt --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/matrix2meme --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/meme-chip_html_to_tsv --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/meme-rename --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/meme_xml_to_html --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/nmica2meme --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/priority2meme --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/prosite2meme --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/psp-gen --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/rna2meme --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/rsat-retrieve-seq --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/rsat-supported-organisms --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/scpd2meme --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/simplepp --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/sites2meme --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/streme_xml_to_html --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/taipale2meme --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/tamo2meme --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/tomtom_xml_to_html --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/transfac2meme --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/uniprobe2meme --prefix PERL5LIB : "${myperl}"
+      wrapProgram $out/libexec/meme-${version}/xstreme_html_to_tsv --prefix PERL5LIB : "${myperl}"
     '';
 
     meta = with lib; {
