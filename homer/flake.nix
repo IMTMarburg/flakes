7c84c116b6cad9091b1b250d57315377fe5cc903
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2025-11-06 14:20:07 +0100

    Added older homer version

--- homer/flake.nix
+++ homer/flake.nix
@@ -9,9 +9,9 @@
     { self, nixpkgs }:
     let
       pkgs = import nixpkgs { system = "x86_64-linux"; };
-      src = pkgs.fetchurl {
-        url = "http://homer.ucsd.edu/homer/data/software/homer.v5.1.zip";
-        sha256 = "sha256-q6pqHpqN+QKkI/tAPdLXqFo893BqtK4JnkpM+w5iGxI=";
+      version_hashes = {
+        "5.1" = "sha256-q6pqHpqN+QKkI/tAPdLXqFo893BqtK4JnkpM+w5iGxI=";
+        "4.11.1" = "sha256-gNHNAGFnKYlAF7JKNqLvgfnN6L02Toda6tHgz7UAyCs=";
       };
 
       configfile = pkgs.writeText "homer-config.txt" ''
@@ -31,51 +31,58 @@
         SETTINGS
       '';
 
+      homer =
+        version:
+        pkgs.stdenv.mkDerivation {
+          pname = "homer";
+          inherit version;
+
+          src = pkgs.fetchurl {
+            url = "http://homer.ucsd.edu/homer/data/software/homer.v${version}.zip";
+            sha256 = version_hashes.${version} or pkgs.lib.fakeSha256;
+          };
+
+          buildInputs = [
+            pkgs.unzip
+            pkgs.gnutar
+            pkgs.gcc
+            pkgs.stdenv.cc.cc.lib
+            pkgs.perl538
+            pkgs.autoconf
+
+          ];
+          nativeBuildInputs = [ pkgs.autoPatchelfHook ];
+
+          unpackPhase = ''
+            mkdir -p $out
+            unzip $src -d $out
+            rm $out/cpp/backup -r
+            cp ${configfile} $out/config.txt
+          '';
+
+          buildPhase = ''
+            perl $out/configureHomer.pl -make
+          '';
+
+          installPhase = ''
+              # we need to fix the perl home paths in all the  scripts.
+              # use lib "/gpfs/data01/cbenner/software/homer/.//bin";
+              #sed -i "s|/gpfs/data01/cbenner/software/homer/|$out/|g" $out/bin/*.pl $out/update/*.pl $out/bin/*.pm $out/bin/old/*.pl $out/cpp/*.cpp
+            # No additional installation steps needed as we already unzipped to $out
+          '';
+
+          meta = with pkgs.lib; {
+            description = "HOMER: Software for Motif Discovery and Next-Gen Sequencing Analysis";
+            homepage = "http://homer.ucsd.edu/homer/";
+            license = licenses.gpl3Plus;
+            maintainers = [ ];
+            platforms = platforms.linux;
+          };
+        };
     in
     {
-      defaultPackage.x86_64-linux = pkgs.stdenv.mkDerivation {
-        pname = "homer";
-        version = "5.1";
-
-        src = src;
-
-        buildInputs = [
-          pkgs.unzip
-          pkgs.gnutar
-          pkgs.gcc
-          pkgs.stdenv.cc.cc.lib
-          pkgs.perl538
-          pkgs.autoconf
-
-        ];
-        nativeBuildInputs = [ pkgs.autoPatchelfHook ];
-
-        unpackPhase = ''
-          mkdir -p $out
-          unzip $src -d $out
-          rm $out/cpp/backup -r
-          cp ${configfile} $out/config.txt
-        '';
-
-        buildPhase = ''
-          perl $out/configureHomer.pl -make
-        '';
-
-        installPhase = ''
-            # we need to fix the perl home paths in all the  scripts.
-            # use lib "/gpfs/data01/cbenner/software/homer/.//bin";
-            #sed -i "s|/gpfs/data01/cbenner/software/homer/|$out/|g" $out/bin/*.pl $out/update/*.pl $out/bin/*.pm $out/bin/old/*.pl $out/cpp/*.cpp
-          # No additional installation steps needed as we already unzipped to $out
-        '';
-
-        meta = with pkgs.lib; {
-          description = "HOMER: Software for Motif Discovery and Next-Gen Sequencing Analysis";
-          homepage = "http://homer.ucsd.edu/homer/";
-          license = licenses.gpl3Plus;
-          maintainers = with maintainers; [ ];
-          platforms = platforms.linux;
-        };
-      };
+      packages.x86_64-linux.default = homer "5.1";
+      packages.x86_64-linux.homer_4-11-1 = homer "4.11.1";
 
     };
 }
